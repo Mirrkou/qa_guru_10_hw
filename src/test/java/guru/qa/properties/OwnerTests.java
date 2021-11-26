15ea1aaad16b2017c8d8d8a2470708cf0e4ab9f1
Mirka <miragaleeva8@gmail.com>
2021-11-26 20:18:34 +0300

    hw11 added

--- src/test/java/guru/qa/properties/OwnerTests.java
+++ src/test/java/guru/qa/properties/OwnerTests.java
@@ -4,10 +4,11 @@ import static java.lang.String.format;
 
 import guru.qa.config.CredentialsConfig;
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+@Tag("properties")
 public class OwnerTests {
-
     public CredentialsConfig credentials =
             ConfigFactory.create(CredentialsConfig.class);
 
